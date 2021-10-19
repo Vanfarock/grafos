4ac736dc8154ce4731d64fb65d834b44b4c89508
Vinícius Manuel Martins <vinicius.m.m.2002@gmail.com>
2021-10-19 16:42:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,112 +1,17 @@
-from graph import Graph
+from questao2 import Questao2
 
-def tipoDoGrafo():
-    pass
-
-def ehDirigido(matriz):
-      n = len(matriz)
-      for lin in range(n):
-        for col in range(n):
-          if (matriz[lin][col] != matriz[col][lin]):
-            return True
-      return False
-
-def ehSimples(matriz):
-    n = len(matriz)
-    for lin in range(n):
-        for col in range(n):
-            if (matriz[lin][lin] > 0 or matriz[lin][col] > 1):
-                return False
-    return True
-
-def ehRegular(matriz):
-    n = len(matriz)
-    for lin in range(n):
-        pass
-
-def getGrauVerticeLinha(matriz, indice):
-    return sum([2 if i == indice else 1 for i, col in enumerate(matriz[indice]) if col > 0])
-
-def getGrauVerticeColuna(matriz, indice):
-    return sum([lin[indice] for lin in matriz if lin[indice] > 0])
-
-def grausDoVertice(matriz):
-    if (ehDirigido(matriz)):
-        return grausDoVerticeDigrafo(matriz)
-    return grausDoVerticeGrafo(matriz)
-
-def grausDoVerticeDigrafo(matriz):
-    grausEntrada = {}
-    grausSaida = {}
-
-    n = len(matriz)
-    for i in range(n):
-        grausEntrada[i] = getGrauVerticeColuna(matriz, i)
-        grausSaida[i] = getGrauVerticeLinha(matriz, i)
-    
-    retorno = f"Sequencia de graus de entrada: {', '.join(getSequenciaGraus(grausEntrada))}\r\n"
-    retorno += f"Sequencia de graus de saida: {', '.join(getSequenciaGraus(grausSaida))}\r\n"
-    retorno += f"Grau de entrada de cada vertice:\r\n"
-    retorno += "\r\n".join(getListaGraus(grausEntrada))
-    retorno += f"\r\nGrau de saida de cada vertice:\r\n"
-    retorno += "\r\n".join(getListaGraus(grausSaida))
-    
-    return retorno
-
-def grausDoVerticeGrafo(matriz):
-    graus = {}
-
-    n = len(matriz)
-    for lin in range(n):
-        graus[lin] = getGrauVerticeLinha(matriz, lin)
-    
-    retorno = f"Sequencia de graus: {', '.join(getSequenciaGraus(graus))}\r\n"
-
-    retorno += "Grau de cada vertice:\r\n"
-    retorno += "\r\n".join(getListaGraus(graus))
-    
-    return retorno
-
-def getSequenciaGraus(graus):
-    grausOrdenados = sorted(graus.items(), key=lambda item : item[1])
-    return [str(v) for _, v in grausOrdenados]
-
-def getListaGraus(graus):
-    return [f"{k} - {v}" for k, v in graus.items()]
-
-def arestasDoGrafo(matriz):
-    n = len(matriz)
-    somaGraus = sum([getGrauVerticeLinha(matriz, i) for i in range(n)])
-    
-    if (ehDirigido(matriz)):
-        return arestasDoGrafoDirigido(matriz, somaGraus)
-    return arestasDoGrafoNaoDirigido(matriz, int(somaGraus / 2))
-
-def arestasDoGrafoDirigido(matriz, quantidadeArestas):
-    retorno = f"Quantidade de arestas: {quantidadeArestas}\r\n"
-    retorno += "Conjunto de arestas:\r\n"
-    
-    retorno += 
-
-
-def arestasDoGrafoNaoDirigido(matriz, quantidadeArestas):
-    pass
 
 def main():
-    matriz = [[0, 1, 0, 1],
-              [1, 0, 1, 1],
-              [0, 1, 0, 1],
-              [1, 1, 1, 0]]
+    questao2 = Questao2()
+    atual = questao2.iniciar()
 
-    print(grausDoVertice(matriz))
-    print(arestasDoGrafo(matriz))
-    # graph = Graph(
-    #   [[0, 1, 0],
-    #    [1, 0, 1],
-    #    [0, 1, 0]])
-    # print(graph)
-    # print(graph.getGraphEdgeType().name)
-    # print(graph.getEdgesCount())
+    trace = []
+    while atual != None:
+        trace.append(atual)
+        atual = atual.pai
+
+    for step in trace[::-1]:
+        print(step)
 
 
 if __name__ == "__main__":
---
+++ questao2.py
@@ -0,0 +1,84 @@
+class Questao2:
+  def __init__(self):
+
+    self.nos = [
+      [No(9),  No(8),    No(7),    No(6), No(5),    No(4),    No(3)],
+      [No(10), No(9),    No(8),    No(7), No(None), No(None), No(2)],
+      [No(11), No(None), No(None), No(8), No(None), No(0),    No(1)],
+      [No(12), No(11),   No(10),   No(9), No(None), No(1),    No(2)],
+      [No(11), No(None), No(None), No(8), No(None), No(None), No(3)],
+      [No(10), No(9),    No(8),    No(7), No(7),    No(5),    No(4)],
+      [No(11), No(None), No(None), No(8), No(6),    No(6),    No(5)],
+    ]
+    
+    for lin in range(len(self.nos)):
+      for col in range(len(self.nos)):
+        self.nos[lin][col].posicao = (lin, col)
+
+    self.inicio = self.nos[2][3]
+    self.destino = self.nos[2][5]
+
+  def iniciar(self):
+    aberto = [self.inicio]
+    fechado = []
+    self.inicio.funcao = 0
+
+    while len(aberto) > 0:
+      atual = self.menor_aberto(aberto)
+      aberto.remove(atual)
+      fechado.append(atual)
+
+      if atual == self.destino:
+        return atual
+
+      for noAdjacente in self.obter_nos_adjacentes(atual):
+        if noAdjacente.eh_obstaculo():
+          continue
+
+        if noAdjacente in fechado:
+          continue
+
+        noAdjacente.pai = atual
+        noAdjacente.distancia = atual.distancia + 1
+        noAdjacente.funcao = noAdjacente.distancia + noAdjacente.heuristica
+
+        if noAdjacente in aberto:
+          continue
+
+        aberto.append(noAdjacente)
+    return None
+
+  def menor_aberto(self, aberto):
+    return min(aberto, key=lambda no: no.funcao)
+
+  def obter_nos_adjacentes(self, atual):
+    n = len(self.nos)
+    direcoes = [
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+      [0, -1],
+    ]
+
+    movimentos = []
+    for direcao in direcoes:
+      dx = atual.posicao[0] + direcao[0]
+      dy = atual.posicao[1] + direcao[1]
+      if dx >= 0 and dx < n and dy >= 0 and dy < n:
+        movimentos.append(self.nos[dx][dy])
+    return movimentos
+
+class No:
+  def __init__(self, heuristica):
+    self.heuristica = heuristica
+    
+    self.posicao = (None, None)
+    self.pai = None
+    self.distancia = 0
+    self.funcao = 0
+
+  def eh_obstaculo(self):
+    return self.heuristica == None
+
+  def __str__(self):
+    return f"{self.posicao} - {self.heuristica}"
